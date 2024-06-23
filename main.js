7d6c3ae62d35e32cccec0fd7450af7c2be4a6e42
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-06-24 02:17:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -81,8 +81,141 @@
 //яка шукає найменше число в масиві.
 //Додай перевірку що функція отримує масив
 
-const numbers = [2, 5, 35, 56, 12, 1, 24, 7, 80, 3];
+// const numbers = [2, 5, 35, 56, 12, 1, 24, 7, 80, 3];
 
-function findSmallerNumber(array) {
-  let;
+// function findSmallerNumber() {
+//   let;
+// }
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+
+// for (const key in apartment) {
+//   keys.push(key);
+// }
+
+// for (const key in apartment) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(keys);
+// console.log(values);
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+
+// for (let key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values);
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   let result = 0;
+
+//   for (let item of products) {
+//     if (item.name === productName) {
+//       result = item.price;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(getProductPrice("Radar")); // Виведе: 1300
+// console.log(getProductPrice("Scanner")); // Виведе: 2700
+// console.log(getProductPrice("Droid")); // Виведе: 400
+// console.log(getProductPrice("Grip")); // Виведе: 1200
+// console.log(getProductPrice("Unknown")); // Виведе: 0, тому що продукт не знайдено
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   let result = 0;
+
+//   for (let item of products) {
+//     if (item.name === productName) {
+//       result = item.price;
+//     }
+//   }
+//   return result;
+// }
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   let result = [];
+
+//   for (let item of products) {
+//     if (propName === "name") {
+//       result.push(item[propName]);
+//     } else if (propName === "price") {
+//       result.push(item[propName]);
+//     } else if (propName === "quantity") {
+//       result.push(item[propName]);
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(getAllPropValues("price"));
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     this.potions.push(newPotion);
+//   },
+//   getTotalPrice() {
+//     let total = 0;
+//     for (let item of this.potions) {
+//       total += item.price;
+//     }
+//     return total;
+//   },
+// };
+
+function addOverNum(value, ...args) {
+  let res = 0;
+  for (let arg of args) {
+    if (arg > value) {
+      res += arg;
+    }
+  }
+  return res;
 }
+
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
